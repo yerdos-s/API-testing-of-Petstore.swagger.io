86b655cf6f9062e60470f7232b2682f138e642cd
Yerdos Sadvakassov <erdos.sadvakasov@gmail.com>
2022-10-26 01:50:09 +0600

    Added Classes to every test file

--- test_get_pet_by_id.py
+++ test_get_pet_by_id.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 import json
 
@@ -6,34 +7,31 @@ with open(json_file, "r") as file:
     request_body = json.load(file)
     object_id = request_body['id']
 
+class TestGetPetByID:
 
-def test_status_code_is_200():
-    response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
-    print(response.json())
-    print(response.headers)
-    assert response.status_code == 200, 'The status code is not 200'
+    def test_status_code_is_200(self):
+        response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
+        print(response.json())
+        print(response.headers)
+        assert response.status_code == 200, 'The status code is not 200'
 
+    def test_content_type_in_response_headers(self):
+        response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
+        assert response.headers['Content-Type'] == response.headers['Content-Type'], \
+            f"The content-type in response headers is not {response.headers['Content-Type']}"
 
-def test_content_type_in_response_headers():
-    response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
-    assert response.headers['Content-Type'] == response.headers['Content-Type'], \
-        f"The content-type in response headers is not {response.headers['Content-Type']}"
+    def test_id_in_response_body(self):
+        response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
+        response_body = response.json()
+        assert response_body['id'] == object_id, 'The id in response body is wrong'
 
+    def test_name_in_response_body(self):
+        response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
+        response_body = response.json()
+        assert response_body['name'] == request_body['name'], 'The name in response body is wrong'
 
-def test_id_in_response_body():
-    response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
-    response_body = response.json()
-    assert response_body['id'] == object_id, 'The id in response body is wrong'
-
-
-def test_name_in_response_body():
-    response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
-    response_body = response.json()
-    assert response_body['name'] == request_body['name'], 'The name in response body is wrong'
-
-
-def test_status_in_response_body():
-    response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
-    response_body = response.json()
-    assert response_body['status'] == request_body['status'], \
-        f"The status in response body is wrong, should be {request_body['status']}"
+    def test_status_in_response_body(self):
+        response = requests.get(f'https://petstore.swagger.io/v2/pet/{object_id}', timeout=10)
+        response_body = response.json()
+        assert response_body['status'] == request_body['status'], \
+            f"The status in response body is wrong, should be {request_body['status']}"
--- test_update_pet.py
+++ test_update_pet.py
@@ -6,39 +6,36 @@ with open(json_file, 'r') as file:
     request_body = json.load(file)
 
 
-def test_status_code_is_200():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    assert response.status_code == 200, 'The status code is not 200'
+class TestUpdatePet:
 
+    def test_status_code_is_200(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        assert response.status_code == 200, 'The status code is not 200'
 
-def test_content_type_in_response_headers():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    assert response.headers['Content-Type'] == response.headers['Content-Type'], \
-        f"The content-type in response headers is not {response.headers['Content-Type']}"
+    def test_content_type_in_response_headers(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        assert response.headers['Content-Type'] == response.headers['Content-Type'], \
+            f"The content-type in response headers is not {response.headers['Content-Type']}"
 
+    def test_id_in_response_body(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        response_body = response.json()
+        assert response_body['id'] == request_body['id'], 'The response id is wrong'
 
-def test_id_in_response_body():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    response_body = response.json()
-    assert response_body['id'] == request_body['id'], 'The response id is wrong'
+    def test_name_in_response_body(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        response_body = response.json()
+        assert response_body['category']['name'] == request_body['category']['name'], \
+            "The name in response body is wrong"
 
+    def test_photo_url_in_response_body(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        response_body = response.json()
+        assert response_body["photoUrls"] == request_body["photoUrls"], \
+            'The photo url is wrong in response body'
 
-def test_name_in_response_body():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    response_body = response.json()
-    assert response_body['category']['name'] == request_body['category']['name'], \
-        "The name in response body is wrong"
-
-
-def test_photo_url_in_response_body():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    response_body = response.json()
-    assert response_body["photoUrls"] == request_body["photoUrls"], \
-        'The photo url is wrong in response body'
-
-
-def test_status_in_response_body():
-    response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
-    response_body = response.json()
-    assert response_body['status'] == request_body['status'], \
-        'The status in response body is wrong'
+    def test_status_in_response_body(self):
+        response = requests.put('https://petstore.swagger.io/v2/pet', json=request_body, timeout=10)
+        response_body = response.json()
+        assert response_body['status'] == request_body['status'], \
+            'The status in response body is wrong'
